99c040fea606c969c74d9a1ba885ac85db560bfa
Francesca Obordo <fobordo@gmail.com>
2021-12-16 00:14:35 -0500

    Created the welcome and precipitation Flask routes.

--- app.py
+++ app.py
@@ -1,8 +1,65 @@
-from flask import Flask
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set up the database
+
+# Allow access to and ability to query the SQLite database file
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect the database into our classes
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Create a variable for each of the classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link from Python to the SQLite database
+session = Session(engine)
+
+# Set up Flask
 app = Flask(__name__)
 
+# Define the welcome route (homepage)
 @app.route('/')
-def hello_world():
-    return 'Hello world'
 
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
+    /api/v1.0/temp/start/end<br>
+    ''')
+
+# Create the precipitation route
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    
+    # Calculates the date one year ago from the most recent date in the database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Get the date and precipitation for the previous year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+    
+    # Get query results into a dictionary format
+    precip = {date: prcp for date, prcp in precipitation}
+    
+    # Format query results into a JSON structured file
+    return jsonify(precip)
+        

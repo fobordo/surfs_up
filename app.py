5c6194b9d5838b8630a973b181a5847000d2d01c
Francesca Obordo <fobordo@gmail.com>
2021-12-16 20:55:32 -0500

    Created the stations, monthly temperature, and statistics Flask routes.

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ session = Session(engine)
 # Set up Flask
 app = Flask(__name__)
 
-# Define the welcome route (homepage)
+# Create the welcome route (homepage)
 @app.route('/')
 
 def welcome():
@@ -55,7 +55,7 @@ def precipitation():
     
     # Get the date and precipitation for the previous year
     precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= prev_year).all()
+        filter(Measurement.date >= prev_year).all()
     
     # Get query results into a dictionary format
     precip = {date: prcp for date, prcp in precipitation}
@@ -63,3 +63,51 @@ def precipitation():
     # Format query results into a JSON structured file
     return jsonify(precip)
         
+# Create the stations route
+@app.route("/api/v1.0/stations")
+
+def stations():
+    
+    # Get all the stations in the database
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+ 
+# Create the monthly temperature route
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    
+    # Calculate the date one year ago from the last date in the database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Query the primary station for all the temperature observations from the previous year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    
+    # Unravel the results into a one-dimensional array and convert that array into a list
+    temps = list(np.ravel(results))
+    
+    # jsonify the list and return the results
+    return jsonify(temps=temps)
+
+# Create the statistics route
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    # Determine the starting and ending date
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+    
+    # Calculate the temperature minimum, average, and maximum with the start and end dates
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
